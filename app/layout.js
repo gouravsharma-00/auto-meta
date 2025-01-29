5d7a7046610d72bb663a991412883258d6018240
Gourav sharma <gourav.sharma.glamat2932@gmail.com>
2025-01-29 16:19:26 +0530

    opengraph tags

--- app/layout.js
+++ app/layout.js
@@ -10,12 +10,18 @@ export default function RootLayout({ children }) {
                 <meta name="description" content="automate your professional life with auto-meta " />
                 <meta name="keywords" content="automate, meta, facebook, Instagram, meta, api, professional, business" />
                 <meta httpEquiv="Content-language" content="en" />
+                {/* canonical */}
+                <link rel='canonical' href='https://auto-meta-pi.vercel.app/' />
                 {/* Opengraph */}
-
+                <meta property='og:title' content='auto-meta' />
+                <meta property='og:description' content='automate your professional life with auto-meta' />
+                <meta property='og:url' content='https://auto-meta-pi.vercel.app/' />
+                <meta property='og:image' content='https://auto-meta-pi.vercel.app/icon.png' />
+                <meta property='og:type' content='website' />
                 {/* Icon */}
-
+                <link rel="icon" href="/favicon.ico" />
                 {/* Title */}
-                <title>auto-mate</title></head>
+                <title>auto-meta</title></head>
             <body>
                 <Header />
                 <main>
